d37c1415d6e66ae27e16f1845126d300e0de8b4d
avrj <anraiha@cs.helsinki.fi>
2015-04-25 21:03:53 +0300

    setup code coverage for code climate

--- Gemfile
+++ Gemfile
@@ -47,6 +47,8 @@ gem 'email_validator'
 
 gem 'rails-i18n'
 
+gem 'codeclimate-test-reporter', group: :test, require: nil
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,8 @@
 require 'simplecov'
 SimpleCov.start('rails')
 require 'capybara/rspec'
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.Start
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
