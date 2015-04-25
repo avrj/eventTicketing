f56f78e47e61562d9d522eeff04c5003bdda9e71
avrj <anraiha@cs.helsinki.fi>
2015-04-25 13:39:05 +0300

    attempt to fix migration error in heroku

--- db/migrate/20150424085943_change_age_to_date_of_birth_in_customer.rb
+++ db/migrate/20150424085943_change_age_to_date_of_birth_in_customer.rb
@@ -1,6 +1,5 @@
 class ChangeAgeToDateOfBirthInCustomer < ActiveRecord::Migration
   def change
 	rename_column :customers, :age, :date_of_birth
-	change_column :customers, :date_of_birth, :datetime
   end
 end
